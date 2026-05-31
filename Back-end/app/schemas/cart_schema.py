33eb7eb2db33c426a755c909006f7d454a62fb9b
aamirmalik75 <aamirmalik7560255@gmail.com>
2026-05-31 16:46:08 +0500

    Added Cart and Cart Item routes and schemas

--- Back-end/app/schemas/cart_schema.py
+++ Back-end/app/schemas/cart_schema.py
@@ -8,29 +8,59 @@ from pydantic import BaseModel, Field
 from schemas.product_schema import ProductOut
 
 
+class SelectedOptionOut(BaseModel):
+    config_id: UUID
+    option_id: UUID
+
+    class Config:
+        from_attributes = True
+
+
+class VariantCartOut(BaseModel):
+    id: UUID
+    color: str
+    stock: int
+
+    class Config:
+        from_attributes = True
+
+
 class CartItemOut(BaseModel):
     id: UUID
-    quantity: int
     product: ProductOut
-    variant_id: Optional[UUID] = None
-    selected_options: dict[str, Any] = Field(default_factory=dict)
+    variant: Optional[VariantCartOut] = None
+    selected_options: list[SelectedOptionOut] = Field(default_factory=list)
+    quantity: int
     final_price: Decimal
-    created_at: datetime
-
-
-class CartOut(BaseModel):
-    id: UUID
-    user_id: UUID
-    items: list[CartItemOut]
-    total_price: Decimal
+    item_total: Decimal
     created_at: datetime
 
     class Config:
         from_attributes = True
 
 
+class CartOut(BaseModel):
+    id: UUID
+    user_id: UUID
+    grand_total: Decimal
+    items: list[CartItemOut] = Field(default_factory=list)
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class selectedOption(BaseModel):
+    config_id: UUID
+    option_id: UUID
+
+
 class CartItemCreate(BaseModel):
     product_id: UUID
     variant_id: UUID
-    selected_options: dict[str, Any] = Field(default_factory=dict)
+    selected_options: list[selectedOption]
     quantity: int = Field(default=1, ge=1)
+
+
+class UpdateQuantity(BaseModel):
+    quantity: int = Field(gt=0)
